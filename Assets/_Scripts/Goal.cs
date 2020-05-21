c1980b626c5693bf73408ee40097ba34e1e25340
Gil Robern <gil.robern@uoit.net>
2020-05-21 17:38:40 -0400

    Basic Goal Check
    If the pickup object is on the goal pad, goal reached is true

---
+++ Assets/_Scripts/Goal.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Goal : MonoBehaviour
+{
+    //Variables
+    private GameObject obj;
+
+    private bool goalReached;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        goalReached = false;
+        obj = GameObject.FindGameObjectWithTag("Pickup");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        goalReached = true;
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        goalReached = false;
+    }
+}
