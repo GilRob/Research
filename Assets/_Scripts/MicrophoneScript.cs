bfb4c5a34c7c02481a59faa939f1fb0b6d920784
Gil Robern <gil.robern@uoit.net>
2020-06-18 17:53:43 -0400

    Microphone Testing
    Turns out WebGL does not support this

---
+++ Assets/_Scripts/MicrophoneScript.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MicrophoneScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        /*AudioSource source = GetComponent<AudioSource>();
+        source.clip = Microphone.Start("Desktop Microphone (4- Microsoft® LifeCam HD-3000)", true, 10, 44100);
+        source.Play();*/
+        /*foreach (var device in Microphone.devices)
+        {
+            Debug.Log("Name: " + device);
+        }*/
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
